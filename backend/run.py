2aeafe0453087f2bf5346f6d6df233cd432668d5
Naufal Najmi <naufalnajmi04@gmail.com>
2026-01-14 12:48:24 +0700

    update code run.py

--- backend/run.py
+++ backend/run.py
@@ -5,4 +5,4 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
