cf48e901222338d9bccdf56d736e7a08533834b8
Naufal Najmi <naufalnajmi04@gmail.com>
2026-01-15 11:52:10 +0700

    update deploy app with docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // ... konfigurasi lain (jika ada) ...
   server: {
-    host: true,       // ✅ WAJIB: Izinkan akses IP eksternal (Docker)
+    host: true,       
     strictPort: true,
-    port: 5173,       // Port standar Vite
+    port: 5173,       
     watch: {
-      usePolling: true, // ✅ PENTING untuk Windows: Biar kalau save file, browser auto-refresh
+      usePolling: true, 
     },
   },
 })
